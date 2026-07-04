e9904837638b76a48fb48c5afbd2427f2a8bc714
Hamim <hamimsiddiq915@gmail.com>
2026-07-04 23:51:49 +0600

    navbar

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,11 @@ import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"], weight: ["400", "500", "600", "700"],
+  subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-outfit",
 });
 
 const ovo = Ovo({
-  subsets: ["latin"], weight: ["400"],
+  subsets: ["latin"], weight: ["400"], variable: "--font-ovo",
 });
 
 export const metadata = {
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${outfit.className} ${ovo.className} h-full antialiased`}
+      className={`${outfit.variable} ${ovo.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        <a href="#top">
+            <Image src={assets.logo} alt='logo' className='w-28 cursor-pointer mr-14'/>
+        </a>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <li><a className='font-ovo' href="#top">Home</a></li>
+            <li><a className='font-ovo' href="#about">About me</a></li>
+            <li><a className='font-ovo' href="#services">Services</a></li>
+            <li><a className='font-ovo' href="#work">My Work</a></li>
+            <li><a className='font-ovo' href="#contact">Contact</a></li>
+        </ul>
+        <div className="">
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo'>Contact <Image src={assets.arrow_icon} alt='arrow' className='w-3'/></a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar

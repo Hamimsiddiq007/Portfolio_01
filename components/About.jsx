ee1757b41450feb1323a5d3cfd521831fd47bb99
Hamim <hamimsiddiq915@gmail.com>
2026-07-05 22:30:32 +0600

    feat: about

---
+++ components/About.jsx
@@ -0,0 +1,23 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-full px-[12%] py-2 scroll mt-20'>
+      <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
+      <h2 className='text-center text-5xl font-ovo'>About me</h2>
+
+      <div className="flex flex-col w-full lg:flex-row items-center gap-20 my-20">
+        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+            <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
+        </div>
+        <div className="flex-1">
+            <p className='mb-10 max-w-2xl font-ovo'>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About

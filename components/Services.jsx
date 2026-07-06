3ccfe2627ffb70be178471f9ab4c88df79cf94fd
Hamim <hamimsiddiq915@gmail.com>
2026-07-06 22:58:42 +0600

    feat: services

---
+++ components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div id='services' className='w-full px-[12%] scroll mt-20'>
+      <h4 className='text-center mb-2 text-lg font-ovo'>What I Offer</h4>
+      <h2 className='text-center text-5xl font-ovo'>My Services</h2>
+
+      <p className='text-center mx-auto max-w-2xl mt-5 mb-12 font-ovo'>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+    </div>
+  )
+}
+
+export default Services

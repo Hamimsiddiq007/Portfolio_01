a243a736e9afeb035490f73f0645e95916401140
Hamim <hamimsiddiq915@gmail.com>
2026-07-07 23:17:42 +0600

    work card css

--- components/Work.jsx
+++ components/Work.jsx
@@ -10,15 +10,17 @@ const Work = () => {
 
       <p className='text-center mx-auto max-w-2xl mt-5 mb-12 font-ovo'>Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
 
-      <div className="">
+      <div className="grid lg:grid-cols-4 grid-cols-1 gap-5 my-10">
         {workData.map((project, index) => (
-            <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className="">
-                <div className="">
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                </div>
-                <div className="">
-                    <Image src={assets.send_icon} alt='send icon' className='w-5'/>
+            <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+                <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                    <div className="">
+                        <h2 className='font-semibold'>{project.title}</h2>
+                        <p className='text-sm text-gray-700'>{project.description}</p>
+                    </div>
+                    <div className="border border-black rounded-full w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                        <Image src={assets.send_icon} alt='send icon' className='w-5'/>
+                    </div>
                 </div>
             </div>
         ))}

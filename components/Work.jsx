53aa9ca0d338be020dfd022821631a951d297039
Hamim <hamimsiddiq915@gmail.com>
2026-07-07 22:57:24 +0600

    feat: my work

---
+++ components/Work.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div id='work' className='w-full px-[12%] scroll mt-20'>
+      <h4 className='text-center mb-2 text-lg font-ovo'>My Portfolio</h4>
+      <h2 className='text-center text-5xl font-ovo'>My latest work</h2>
+
+      <p className='text-center mx-auto max-w-2xl mt-5 mb-12 font-ovo'>Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
+    </div>
+  )
+}
+
+export default Work

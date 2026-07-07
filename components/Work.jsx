c3cea42b70d175d7a8f47f81d80d8c4b66374c32
Hamim <hamimsiddiq915@gmail.com>
2026-07-07 23:04:42 +0600

    work card

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,3 +1,5 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Work = () => {
@@ -7,6 +9,20 @@ const Work = () => {
       <h2 className='text-center text-5xl font-ovo'>My latest work</h2>
 
       <p className='text-center mx-auto max-w-2xl mt-5 mb-12 font-ovo'>Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
+
+      <div className="">
+        {workData.map((project, index) => (
+            <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className="">
+                <div className="">
+                    <h2>{project.title}</h2>
+                    <p>{project.description}</p>
+                </div>
+                <div className="">
+                    <Image src={assets.send_icon} alt='send icon' className='w-5'/>
+                </div>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }

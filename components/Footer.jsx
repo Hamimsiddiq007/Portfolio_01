dfb3e36a8e4f430c5026f2ab51cd32db9ae8959c
Hamim <hamimsiddiq915@gmail.com>
2026-07-08 21:41:43 +0600

    feat: footer

---
+++ components/Footer.jsx
@@ -0,0 +1,29 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+      <div className="text-center">
+        <Image src={assets.logo} alt='logo' className='w-36 mx-auto mb-2' />
+
+        <div className="w-max flex items-center gap-2 mx-auto">
+            <Image src={assets.mail_icon} alt='logo' className='w-6' />
+            hamimsiddiq915@gmail.com
+        </div>
+      </div>
+
+      <div className="">
+        <p>@ 2026 Hamim Siddiq. All rights reserved</p>
+        <ul>
+            <li><a target='_blank' href="https://github.com/Hamimsiddiq007">Github</a></li>
+            <li><a target='_blank' href=" https://www.linkedin.com/in/hamim-siddik-6a904b3a9">LinkedIn</a></li>
+            <li><a target='_blank' href="#">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer

fc221592934ec15c03af4e899ef47b2b46db343d
Hamim <hamimsiddiq915@gmail.com>
2026-07-08 20:19:46 +0600

    feat: contact

---
+++ components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      id="contact"
+      className='w-full px-[12%] scroll mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'
+    >
+      <h4 className="text-center mb-2 text-lg font-ovo">Connect with me</h4>
+      <h2 className="text-center text-5xl font-ovo">Get in touch</h2>
+
+      <p className="text-center mx-auto max-w-2xl mt-5 mb-12 font-ovo">
+        I would love to hear from you! If you have any questions, comments or feedback, please use the form below.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;

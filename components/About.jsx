39241c2e3fb8e1f18258bd80dd346dcfc489a05d
Hamim <hamimsiddiq915@gmail.com>
2026-07-09 22:46:52 +0600

    about dark setup

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { assets, infoList, toolsData } from '@/assets/assets'
 import React from 'react'
 
-const About = () => {
+const About = ({isDarkMode}) => {
   return (
     <div id='about' className='w-full px-[12%] scroll mt-20'>
       <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
@@ -17,15 +17,15 @@ const About = () => {
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                 {infoList.map(({icon, iconDark, title, description}, index) => (
-                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 hover:shadow-black' key={index}>
-                        <Image src={icon} alt={title} className='w-7 mt-3'/>
-                        <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
-                        <p className='text-gray-600 text-sm'>{description}</p>
+                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-dark-hover' key={index}>
+                        <Image src={ isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3'/>
+                        <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
+                        <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
                     </li>
                 ))}
             </ul>
 
-            <h4 className='my-6 text-gray-700 font-ovo'>Tools I Use</h4>
+            <h4 className='my-6 text-gray-700 font-ovo dark:text-white/80'>Tools I Use</h4>
             <ul className='flex items-center gap-3 sm:gap-5'>
               {toolsData.map((tool, index) => (
                 <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>

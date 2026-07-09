e4b0f76097dbfce0274a5c4a12e345a42123b43c
Hamim <hamimsiddiq915@gmail.com>
2026-07-09 22:41:03 +0600

    header dark setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
       <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>Frontend web developer based in London.</h1>
       <p className='max-w-2xl mx-auto font-ovo'>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-        <a href="/sample-resume.pdf" download className='px-10 py-3 border border-gray-500 rounded-full flex items-center gap-2'>My resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+        <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+        <a href="/sample-resume.pdf" download className='px-10 py-3 border border-gray-500 rounded-full flex items-center gap-2 bg-white dark:text-black'>My resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
       </div>
     </div>
   )
